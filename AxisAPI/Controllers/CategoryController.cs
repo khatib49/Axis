523d759e8bffe69445734815161ecf6943dec980
mkaraz-icarry <m.karaz@icarry.com>
2025-09-12 10:43:30 +0300

    Added CategoryController

---
+++ AxisAPI/Controllers/CategoryController.cs
@@ -0,0 +1,57 @@
+﻿using Application.DTOs;
+using Application.IServices;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AxisAPI.Controllers
+{
+    [ApiController]
+    [Route("api/category")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> Get(Guid id, CancellationToken ct)
+        {
+            var item = await _categoryService.GetAsync(id, ct);
+            if (item is null) return NotFound();
+            return Ok(item);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List(CancellationToken ct)
+        {
+            var categories = await _categoryService.ListAsync(ct);
+            return Ok(categories);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Update(Guid id, CategoryUpdateDto dto, CancellationToken ct)
+        {
+            var success = await _categoryService.UpdateAsync(id, dto, ct);
+            if (!success) return NotFound();
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryCreateDto dto, CancellationToken ct)
+        {
+            var created = await _categoryService.CreateAsync(dto, ct);
+            return CreatedAtAction(nameof(Get), new { id = created.Id }, created);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id, CancellationToken ct)
+        {
+            var success = await _categoryService.DeleteAsync(id, ct);
+            if (!success) return NotFound();
+            return NoContent();
+        }
+    }
+}
