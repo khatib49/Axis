d9ec900fa23408ae82ba0b6536a3f531ed82424e
Ali Dbouk <dboukali98@outlook.com>
2025-09-20 00:05:50 +0300

    added categoty type in add category

--- Application/DTOs/CategoryItemDtos.cs
+++ Application/DTOs/CategoryItemDtos.cs
@@ -1,7 +1,7 @@
 ﻿namespace Application.DTOs
 {
-    public record CategoryDto(Guid Id, string Name);
-    public record CategoryCreateDto(string Name);
+    public record CategoryDto(Guid Id, string Name , string Type);
+    public record CategoryCreateDto(string Name , string Type);
     public record CategoryUpdateDto(string? Name);
 
     public record ItemDto(Guid Id, string Name, int Quantity, decimal Price, string Type, Guid CategoryId, Guid? StatusId);
