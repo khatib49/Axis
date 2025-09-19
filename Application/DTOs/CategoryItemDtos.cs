afe1ae9c65cee03dc510bf3c4f1dcddd8f929644
Ali Dbouk <dboukali98@outlook.com>
2025-09-20 00:07:45 +0300

    added on edit categoty type

--- Application/DTOs/CategoryItemDtos.cs
+++ Application/DTOs/CategoryItemDtos.cs
@@ -2,7 +2,7 @@
 {
     public record CategoryDto(Guid Id, string Name , string Type);
     public record CategoryCreateDto(string Name , string Type);
-    public record CategoryUpdateDto(string? Name);
+    public record CategoryUpdateDto(string? Name , string Type);
 
     public record ItemDto(Guid Id, string Name, int Quantity, decimal Price, string Type, Guid CategoryId, Guid? StatusId);
     public record ItemCreateDto(string Name, int Quantity, decimal Price, string Type, Guid CategoryId, Guid? StatusId);
