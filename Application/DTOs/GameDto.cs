4e41a39eada3ee2312494528cedcc69e60ef86f7
mkaraz-icarry <m.karaz@icarry.com>
2025-09-29 22:12:39 +0300

    Fetch category

--- Application/DTOs/GameDto.cs
+++ Application/DTOs/GameDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace Application.DTOs
 {
-    public record GameDto(Guid Id, string Name, Guid CategoryId, string CategoryName, Guid? StatusId, DateTime CreatedOn, DateTime? ModifiedOn);
+    public record GameDto(Guid Id, string Name, Guid CategoryId, string CategoryName, Guid? StatusId, string Status, DateTime? CreatedOn, DateTime? ModifiedOn);
     public record GameCreateDto(string Name, Guid CategoryId, Guid? StatusId);
     public record GameUpdateDto(string? Name, Guid? CategoryId, Guid? StatusId);
 }
