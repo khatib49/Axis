4085dc74f46876d1d1cb30bb3d9264b81fc424f5
mkaraz-icarry <m.karaz@icarry.com>
2025-09-29 21:53:58 +0300

    Add Category To Game

--- Domain/Entities/Game.cs
+++ Domain/Entities/Game.cs
@@ -4,7 +4,8 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = default!;
-        public string Type { get; set; } = default!;
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; } = default!;
         public Guid StatusId { get; set; }       
         public Status Status { get; set; } = default!;
         public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
