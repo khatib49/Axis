fcf1a9bedecf811917062cd39e093354cb1afcaa
mkaraz-icarry <m.karaz@icarry.com>
2025-09-29 22:28:37 +0300

    map status

--- Application/Services/GamesService.cs
+++ Application/Services/GamesService.cs
@@ -22,6 +22,7 @@ namespace Application.Services
         {
             var e = await _repo.Query()
                     .Include(s => s.Category)
+                    .Include(s => s.Status)
                     .AsNoTracking()
                     .FirstOrDefaultAsync(s => s.Id == id, ct);
 
@@ -32,6 +33,7 @@ namespace Application.Services
         {
             var list = await _repo.Query()
                         .Include(s => s.Category)
+                        .Include(s=>s.Status)
                         .AsNoTracking()
                         .ToListAsync(ct);
 
