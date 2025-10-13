6f155f3b7234f57401d0995dad845684a2f401d1
Rami Al Khatib <khatibrami8@gmail.com>
2025-10-13 21:04:33 +0300

    Update

--- Application/DTOs/RoomDtos.cs
+++ Application/DTOs/RoomDtos.cs
@@ -1,6 +1,6 @@
 ﻿namespace Application.DTOs
 {
-    public record RoomDto(int Id, string Name, int? CategoryId, string? CategoryName, int? Sets);
-    public record RoomCreateDto(string Name, int? CategoryId, int? Sets);
-    public record RoomUpdateDto(string? Name, int? CategoryId, int? Sets);
+    public record RoomDto(int Id, string Name, int? CategoryId, string? CategoryName, int SetCount);
+    public record RoomCreateDto(string Name, int? CategoryId, int SetCount);
+    public record RoomUpdateDto(string? Name, int? CategoryId, int? SetCount);
 }
