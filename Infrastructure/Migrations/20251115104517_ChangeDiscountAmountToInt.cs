c262beb8f82f0bb2b63c2429c16481d2757d55db
mkaraz-icarry <m.karaz@icarry.com>
2025-11-15 12:55:57 +0200

    Update

---
+++ Infrastructure/Migrations/20251115104517_ChangeDiscountAmountToInt.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDiscountAmountToInt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Discounts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Percentage",
+                table: "Discounts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Percentage",
+                table: "Discounts");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Discounts",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
